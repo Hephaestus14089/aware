dfadc0ad166751714feb77c3154882d864ce3ba5
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2024-03-08 00:35:09 +0530

    move main to cli.js
    src/main.js is rendered unnecessary

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,27 @@
 #! /usr/bin/env node
 
-import main from '../src/main.js';
+import { runSet } from '../src/runFunctions.js';
+import { difficulties } from '../src/customConfig.js';
+import readline from 'node:readline'
+import { stdin, stdout, exit } from 'node:process';
+import { prompt } from '../src/utilities.js';
+
+const readlineInterface = readline.createInterface({ input: stdin, output: stdout });
 
 console.log("A W A R E");
+
+const main = async () => {
+  while (true) {
+    await runSet(difficulties, readlineInterface);
+
+    const query = "\nEnter 'q' to quit and any other key to begin a new set: ";
+    const userInput = await prompt(readlineInterface, query);
+
+    if (userInput.charAt(0) === "q".charAt(0)) {
+      readlineInterface.close();
+      exit(0);
+    }
+  }
+};
 
 main();
